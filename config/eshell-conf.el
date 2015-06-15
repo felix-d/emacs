3ec1f0fcc06fb373035680d815433d2e46a095ec
Felix Descoteaux <flx.descoteaux@gmail.com>
2015-06-15 16:28:32 -0400

    rm bookmarks

--- config/eshell-conf.el
+++
@@ -1,7 +0,0 @@
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin:/Applications/MAMP/Library/bin:"))
-(setq exec-path (append exec-path '("/usr/local/bin")))
-(global-set-key (kbd "<f1>") 'term)
-(setq eshell-cmpl-ignore-case t)
-
-(defalias 'openf 'find-file)
-(defalias 'openfo 'find-file-other-window)
